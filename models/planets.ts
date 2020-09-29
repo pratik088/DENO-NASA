3771db3096f1d08f49be678dc0957ad85ee44525
Pratik Dineshkumar Solanki <pratik.solanki312@gmail.com>
2020-09-29 00:03:45 -0400

    Updated tests

--- models/planets.ts
+++ models/planets.ts
@@ -15,6 +15,18 @@ type Planet = Record<string, string>;
 
 let planets: Array<Planet>;
 
+export function filterHabitablePlanets(planets: Array<Planet>) {
+  return planets.filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
+      stellarMass > 0.78 && stellarMass < 1.04 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01;
+  });
+}
+
 async function loadPlenetData() {
   const path = join("data", "kepler_exoplanets_nasa.csv");
   const file = await Deno.open(path);
@@ -29,16 +41,7 @@ async function loadPlenetData() {
   //  }
 
   Deno.close(file.rid);
-  const planets = (result as Array<Planet>).filter((planet) => {
-    const planetaryRadius = Number(planet["koi_prad"]);
-    const stellarMass = Number(planet["koi_smass"]);
-    const stellarRadius = Number(planet["koi_srad"]);
-    return planet["koi_disposition"] === "CONFIRMED" &&
-      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
-      stellarMass > 0.78 && stellarMass < 1.04 &&
-      stellarRadius > 0.99 && stellarRadius < 1.01;
-  });
-
+  const planets = filterHabitablePlanets (result as Array<Planet>);
   return planets.map((planet) => {
     return pick(planet, [
       "koi_prad",
